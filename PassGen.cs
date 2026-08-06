d59b4f423c8d8dc5c598e3fef4e37ca24e2b9a56
Mr_HedgeHog_UA <60219855+HedgeHog777@users.noreply.github.com>
2026-08-06 17:05:50 +0200

    Update PassGen.cs

--- PassGen.cs
+++ PassGen.cs
@@ -1,23 +1,74 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 
-class PasswordGenerator {
-    static void Main() {
-        int passwordLength = 35;
-        string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*@?";
-        char[] chars = new char[passwordLength];
-        byte[] data = new byte[passwordLength];
+internal static class PasswordGenerator
+{
+    private const string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+    private const string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string Digits = "0123456789";
+    private const string Symbols = "!#$%&*@?";
 
-        using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider()) {
-            crypto.GetBytes(data);
+    public static string Generate(
+        int length = 35,
+        bool useLowercase = true,
+        bool useUppercase = true,
+        bool useDigits = true,
+        bool useSymbols = true)
+    {
+        if (length <= 0)
+            throw new ArgumentOutOfRangeException(nameof(length));
+
+        var characterSets = new List<string>();
+
+        if (useLowercase) characterSets.Add(Lowercase);
+        if (useUppercase) characterSets.Add(Uppercase);
+        if (useDigits) characterSets.Add(Digits);
+        if (useSymbols) characterSets.Add(Symbols);
+
+        if (characterSets.Count == 0)
+            throw new InvalidOperationException("At least one character set must be selected.");
+
+        if (length < characterSets.Count)
+            throw new ArgumentException(
+                "Password length is too short for the selected character sets.");
+
+        var password = new List<char>(length);
+
+        // Гарантуємо хоча б один символ із кожної категорії
+        foreach (string set in characterSets)
+        {
+            password.Add(set[RandomNumberGenerator.GetInt32(set.Length)]);
         }
 
-        for (int i = 0; i < passwordLength; i++) {
-            chars[i] = allowedChars[data[i] % allowedChars.Length];
+        // Загальний набір символів
+        string allCharacters = string.Concat(characterSets);
+
+        while (password.Count < length)
+        {
+            password.Add(allCharacters[
+                RandomNumberGenerator.GetInt32(allCharacters.Length)]);
         }
-        
-        string password = new string(chars);
+
+        // Перемішуємо (Fisher-Yates)
+        for (int i = password.Count - 1; i > 0; i--)
+        {
+            int j = RandomNumberGenerator.GetInt32(i + 1);
+            (password[i], password[j]) = (password[j], password[i]);
+        }
+
+        return new string(password.ToArray());
+    }
+}
+
+internal static class Program
+{
+    static void Main()
+    {
+        string password = PasswordGenerator.Generate(
+            length: 32,
+            useSymbols: true);
+
         Console.WriteLine(password);
     }
 }
